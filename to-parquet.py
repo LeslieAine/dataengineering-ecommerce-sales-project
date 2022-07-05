a0dbc545f8fdd0301a100873014e22df47b621b6
LeslieAine <103054328+LeslieAine@users.noreply.github.com>
2022-07-05 15:44:00 +0300

    Create to-parquet.py

---
+++ to-parquet.py
@@ -0,0 +1,43 @@
+# python script to convert the transformed data to parquet
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+## @type: DataSource
+## @args: [database = "db-ecommerce-raw-sales-data", table_name = "ecommerce-transformed-sales", transformation_ctx = "datasource0"]
+## @return: datasource0
+## @inputs: []
+datasource0 = glueContext.create_dynamic_frame.from_catalog(database = "db-ecommerce-raw-sales-data", table_name = "ecommerce-transformed-sales", transformation_ctx = "datasource0")
+## @type: ApplyMapping
+## @args: [mapping = [("amount", "long", "amount", "long"), ("orderid", "long", "orderid", "long"), ("categoryid", "long", "categoryid", "long"), ("countryid", "long", "countryid", "long"), ("dateid", "long", "dateid", "long"), ("category", "string", "category", "string"), ("country", "string", "country", "string"), ("date", "string", "date", "string"), ("year", "long", "year", "long"), ("quarter", "long", "quarter", "long"), ("quartername", "string", "quartername", "string"), ("month", "long", "month", "long"), ("monthname", "string", "monthname", "string"), ("day", "long", "day", "long"), ("weekday", "long", "weekday", "long"), ("weekdayname", "string", "weekdayname", "string")], transformation_ctx = "applymapping1"]
+## @return: applymapping1
+## @inputs: [frame = datasource0]
+applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("amount", "long", "amount", "long"), ("orderid", "long", "orderid", "long"), ("categoryid", "long", "categoryid", "long"), ("countryid", "long", "countryid", "long"), ("dateid", "long", "dateid", "long"), ("category", "string", "category", "string"), ("country", "string", "country", "string"), ("date", "string", "date", "string"), ("year", "long", "year", "long"), ("quarter", "long", "quarter", "long"), ("quartername", "string", "quartername", "string"), ("month", "long", "month", "long"), ("monthname", "string", "monthname", "string"), ("day", "long", "day", "long"), ("weekday", "long", "weekday", "long"), ("weekdayname", "string", "weekdayname", "string")], transformation_ctx = "applymapping1")
+## @type: ResolveChoice
+## @args: [choice = "make_struct", transformation_ctx = "resolvechoice2"]
+## @return: resolvechoice2
+## @inputs: [frame = applymapping1]
+resolvechoice2 = ResolveChoice.apply(frame = applymapping1, choice = "make_struct", transformation_ctx = "resolvechoice2")
+## @type: DropNullFields
+## @args: [transformation_ctx = "dropnullfields3"]
+## @return: dropnullfields3
+## @inputs: [frame = resolvechoice2]
+dropnullfields3 = DropNullFields.apply(frame = resolvechoice2, transformation_ctx = "dropnullfields3")
+## @type: DataSink
+## @args: [connection_type = "s3", connection_options = {"path": "s3://ecommerce-cleaned-sales-data-parquet/cleaned_data"}, format = "parquet", transformation_ctx = "datasink4"]
+## @return: datasink4
+## @inputs: [frame = dropnullfields3]
+datasink4 = glueContext.write_dynamic_frame.from_options(frame = dropnullfields3, connection_type = "s3", connection_options = {"path": "s3://ecommerce-cleaned-sales-data-parquet/cleaned_data"}, format = "parquet", transformation_ctx = "datasink4")
+job.commit()
